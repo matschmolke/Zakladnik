edd5b16a059820503619d24ce0731f3e2beca5ec
Mateusz <mateuszs_2003@wp.pl>
2025-06-17 15:48:55 +0200

    added CSV export

--- Zakladnik/Pages/Zaklady/Index.cshtml.cs
+++ Zakladnik/Pages/Zaklady/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -48,6 +50,38 @@ namespace Zakladnik.Pages.Zaklady
 
             Zaklad = await query.ToListAsync();
             Bilans = Zaklad.Sum(z => z.FaktycznaWygrana - z.Stawka);
+        }
+
+        //CSV Export
+        public async Task<IActionResult> OnPostExportAsync()
+        {
+            var zaklady = await _context.Zaklady.ToListAsync();
+
+            var csv = new StringBuilder();
+            csv.AppendLine("Id;Data;Bukmacher;Stawka;Kurs;Wygrany;Podatek;EWK;Wygrana");
+
+            foreach (var z in zaklady)
+            {
+                var line = string.Join(";", new[]
+                {
+                    z.Id.ToString(),
+                    z.Data.ToString("yyyy-MM-dd HH:mm"),
+                    z.Bukmacher,
+                    z.Stawka.ToString("0.00", CultureInfo.InvariantCulture),
+                    z.Kurs.ToString("0.00", CultureInfo.InvariantCulture),
+                    z.Wygrany ? "Tak" : "Nie",
+                    z.Podatek.ToString("0.00", CultureInfo.InvariantCulture),
+                    z.PotencjalnaWygrana.ToString("0.00", CultureInfo.InvariantCulture),
+                    z.FaktycznaWygrana.ToString("0.00", CultureInfo.InvariantCulture)
+                });
+
+                csv.AppendLine(line);
+            }
+
+            var bytes = Encoding.UTF8.GetBytes(csv.ToString());
+            var fileName = $"zaklady_{DateTime.Now:yyyyMMdd}.csv";
+
+            return File(bytes, "text/csv", fileName);
         }
     }
 }
